d8029089e4a97ee75cc11eb84a12fc3da28ed942
Akash Singh <akashsingh6280@gmail.com>
2021-10-05 23:49:53 +0530

    ADDED: Calculating wage for a month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,19 +1,10 @@
 const IS_ABSENT=0
-
-let empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck==IS_ABSENT){
-    console.log("Employee is Absent");
-    return
-}
-else{
-    console.log("Employee is Present");
-} 
-
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUMBER_OF_WORKING_DAYS=20;
 
 function getWorkingHours(employeeCheck){
     switch(employeeCheck) {
@@ -26,8 +17,10 @@ function getWorkingHours(employeeCheck){
     }
 }
 
-let employeeHrs = 0;
-let employeeCheck = Math.floor(Math.random() * 10) % 3;
-employeeHrs=getWorkingHours(employeeCheck);
-let employeeWage = employeeHrs * WAGE_PER_HOUR;
-console.log("Employee Wage: "+employeeWage); 
+let totalEmployeeHrs=0;
+for(let day=0;day<NUMBER_OF_WORKING_DAYS;day++){
+    let employeeCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmployeeHrs+=getWorkingHours(employeeCheck);
+}
+let employeeWage = totalEmployeeHrs * WAGE_PER_HOUR;
+console.log("Total Hrs :"+ totalEmployeeHrs+" Employee Wage: "+employeeWage); 
